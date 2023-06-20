1333700c98b589a040e26e2a348b4491f7e1478a
locchuong <locchuong9@gmail.com>
2023-06-20 09:55:58 +0700

    update

--- utils/parse_via.py
+++ utils/parse_via.py
@@ -1,5 +1,23 @@
 import json
 
+# all supported annotation types
+annotation_types = ['rect','polygon','polyline']
+
 # read json file
 with open('via/via_region_data.json') as f:
     json_data = json.load(f)
+
+# list all images at fist level
+images = json_data.keys()
+print('Found ',len(images),' images in json file')
+for image in images:
+    # get current image
+    current_image = json_data[image]
+    
+    # keys =  current_image.keys()
+    # for key in keys:
+    #     print('\t\t',key,':',current_image[key])
+    
+    # get regions
+    regions = current_image['regions']
+    print('\t\tFound ',len(regions),' regions')
