968da380eb98b9351d46bff307a45ca4c0e70676
locchuong123 <locchuong123@gmail.com>
2023-06-21 00:37:12 +0700

    update

--- utils/generate_cvat.py
+++ utils/generate_cvat.py
@@ -10,11 +10,11 @@ root.appendChild(xml)
 # set attribute for element
 productChild = root.createElement('product')
 productChild.setAttribute('name','Geeks for Geeks')
-
 xml.appendChild(productChild)
+
 xml_str = root.toprettyxml(indent='\t')
 
-save_path_file = 'output/gfg.xml'
+save_path_file = 'tmp/gfg.xml'
 
 with open(save_path_file,'w') as f:
     f.write(xml_str)
