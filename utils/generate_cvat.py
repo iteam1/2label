4ed513076c6c7e39f25ded1f1685a3a6b30c7efa
locchuong123 <locchuong123@gmail.com>
2023-06-21 00:34:51 +0700

    update

--- utils/generate_cvat.py
+++ utils/generate_cvat.py
@@ -1,1 +1,20 @@
+import os
 from xml.dom import minidom
+
+root = minidom.Document()
+
+# create tag
+xml = root.createElement('root')
+root.appendChild(xml)
+
+# set attribute for element
+productChild = root.createElement('product')
+productChild.setAttribute('name','Geeks for Geeks')
+
+xml.appendChild(productChild)
+xml_str = root.toprettyxml(indent='\t')
+
+save_path_file = 'output/gfg.xml'
+
+with open(save_path_file,'w') as f:
+    f.write(xml_str)
