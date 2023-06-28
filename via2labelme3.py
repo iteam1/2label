40ac44e197cad1f5222ce248ee97edfda3aa5d52
locchuong <locchuong9@gmail.com>
2023-06-28 10:51:44 +0700

    update

--- via2labelme3.py
+++ via2labelme3.py
@@ -1,3 +1,4 @@
+# python3 via2labelme3.py dataset/via
 import os
 import shutil
 import json
@@ -55,15 +56,131 @@ def gen_xml(img_name,img_width,img_height,regions):
     xml.appendChild(imagesize)
     
     # loop through all regions
-    for region in regions:
+    for i,region in enumerate(regions):
         # create object tag
         object = root.createElement('object')
         # get region label
         img_label = region.get('region_attributes').get('labels')
+        img_type = region.get('shape_attributes').get('name')
         # create name tag
         name = root.createElement('name')
         name.appendChild(root.createTextNode(img_label))
         object.appendChild(name)
+        # create deleted tag
+        deleted = root.createElement('deleted')
+        deleted.appendChild(root.createTextNode('0'))
+        object.appendChild(deleted)
+        # create verified tag
+        verified = root.createElement('verified')
+        verified.appendChild(root.createTextNode('0'))
+        object.appendChild(verified)
+        # create occluded tag
+        occluded = root.createElement('occluded')
+        occluded.appendChild(root.createTextNode('no'))
+        object.appendChild(occluded)
+        # create date tag
+        date = root.createElement('date')
+        date.appendChild(root.createTextNode(''))
+        object.appendChild(date)
+        # create id tag
+        id = root.createElement('id')
+        id.appendChild(root.createTextNode(str(i)))
+        object.appendChild(id)
+        # create parts tag
+        parts = root.createElement('parts')
+        hasparts = root.createElement('hasparts')
+        hasparts.appendChild(root.createTextNode(''))
+        ispartof = root.createElement('ispartof')
+        ispartof.appendChild(root.createTextNode(''))
+        parts.appendChild(hasparts)
+        parts.appendChild(ispartof)
+        object.appendChild(parts)        
+        # create attributes tag and type tag
+        if img_type == 'polygon':
+            # only create attributes tag
+            attributes = root.createElement('attributes')
+            attributes.appendChild(root.createTextNode(''))
+            object.appendChild(attributes)
+        elif img_type == 'rect':
+            # create atrributes tag
+            attributes = root.createElement('attributes')
+            attributes.appendChild(root.createTextNode('rotation=0.0'))
+            object.appendChild(attributes)
+            # crate type tag
+            type = root.createElement('type')
+            type.appendChild(root.createTextNode('bounding_box'))
+            object.appendChild(type)
+        else:
+            print('attributes:',img_type, ' is not supported, continue')
+            continue
+        # create polygon tag
+        polygon = root.createElement('polygon')
+        username = root.createElement('username')
+        username.appendChild(root.createTextNode(''))
+        polygon.appendChild(username)
+        if img_type == "polygon":
+            all_points_x = region.get('shape_attributes').get('all_points_x')
+            all_points_y = region.get('shape_attributes').get('all_points_y')
+            for j in range(len(all_points_x)):
+                # create pt tag
+                pt = root.createElement('pt')
+                x_tag = root.createElement('x')
+                y_tag = root.createElement('y')
+                x = all_points_x[j]
+                y = all_points_y[j]
+                x_tag.appendChild(root.createTextNode(str(int(x))))
+                y_tag.appendChild(root.createTextNode(str(int(y))))
+                pt.appendChild(x_tag)
+                pt.appendChild(y_tag)
+                polygon.appendChild(pt)
+                
+        elif img_type == "rect":
+            x = region.get('shape_attributes').get('x')
+            y = region.get('shape_attributes').get('y')
+            width = region.get('shape_attributes').get('width')
+            height = region.get('shape_attributes').get('height')
+            # top left point
+            pt = root.createElement('pt')
+            x_tag = root.createElement('x')
+            y_tag = root.createElement('y')
+            x_tag.appendChild(root.createTextNode(str(int(x))))
+            y_tag.appendChild(root.createTextNode(str(int(y))))
+            pt.appendChild(x_tag)
+            pt.appendChild(y_tag)
+            polygon.appendChild(pt)
+            # top right point
+            pt = root.createElement('pt')
+            x_tag = root.createElement('x')
+            y_tag = root.createElement('y')
+            x_tag.appendChild(root.createTextNode(str(int(x+width))))
+            y_tag.appendChild(root.createTextNode(str(int(y))))
+            pt.appendChild(x_tag)
+            pt.appendChild(y_tag)
+            polygon.appendChild(pt)
+            # bottom right point
+            pt = root.createElement('pt')
+            x_tag = root.createElement('x')
+            y_tag = root.createElement('y')
+            x_tag.appendChild(root.createTextNode(str(int(x+width))))
+            y_tag.appendChild(root.createTextNode(str(int(y+height))))
+            pt.appendChild(x_tag)
+            pt.appendChild(y_tag)
+            polygon.appendChild(pt)
+            # bottom left point
+            pt = root.createElement('pt')
+            x_tag = root.createElement('x')
+            y_tag = root.createElement('y')
+            x_tag.appendChild(root.createTextNode(str(int(x))))
+            y_tag.appendChild(root.createTextNode(str(int(y)+height)))
+            pt.appendChild(x_tag)
+            pt.appendChild(y_tag)
+            polygon.appendChild(pt)
+        else:
+            print('polygon:',img_type, ' is not supported, continue')
+            continue
+        object.appendChild(polygon)
+            
+        # append object to annotation tag
         xml.appendChild(object)
     
     
@@ -102,7 +219,3 @@ for img_key in img_keys:
     save_path_file = os.path.join(dst,img_name.split('.')[0]+'.xml')
     with open(save_path_file,'w') as f:
         f.write(xml_str)
-    
-    
-
-    
